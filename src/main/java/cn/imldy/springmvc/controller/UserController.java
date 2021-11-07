9818c1acf6bda992fac66061a94e58fd3f5c3f2f
imldy <im.ldy@outlook.com>
2021-11-07 16:26:46 +0800

    13.2.1

---
+++ src/main/java/cn/imldy/springmvc/controller/UserController.java
@@ -0,0 +1,20 @@
+package cn.imldy.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author imldy
+ * @date 2021/11/07 10:55
+ **/
+@Controller
+public class UserController {
+    @RequestMapping("/selectUser")
+    public String selectUser(HttpServletRequest request){
+        String id = request.getParameter("id");
+        System.out.println("id = " + id);
+        return "sucess";
+    }
+}
